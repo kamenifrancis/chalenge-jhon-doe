5be59aeab00a3c3929ce262f0ca07814a219aa74
kamenifrancis <blickson57@gmail.com>
2023-01-30 14:28:50 +0100

    jeu de jhon doe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Heard from './Hearder';
+import Corp from './Corps';
+import Project from './Projects';
+import Contact from './Contacts';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      <Heard/> 
+      <Corp/>
+      <Project/> 
+      <Contact/>
     </div>
   );
 }
---
+++ src/Corps.js
@@ -0,0 +1,13 @@
+const Corp = () =>{
+    return(
+        <section id="about-me">
+        <h1>
+          Hello, my name is
+          <span class="rotate text-important">John doe</span>,<br />
+          and i make horrible websites.
+        </h1>
+        <img class="avatar" src="https://ehqd9.csb.app/images/John-Doe.jpg" alt="jhon-doe" />
+      </section>
+    )
+}
+export default Corp
